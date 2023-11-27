1e197d43de269950e5547e2ea781c33dbdf28291
SebastianReyes <58503819+SebastianReyes19@users.noreply.github.com>
2023-11-27 10:52:58 -0600

    more in depth on checking within rather than self.

--- function_sorter.py
+++ function_sorter.py
@@ -10,16 +10,16 @@
 
 import csv
 import os
-
 from ghidra.program.flatapi import FlatProgramAPI
 
 listing = currentProgram.getListing()
+
+networking_functions = ['socket', 'bind', 'listen', 'accept', 'connect', 'send', 'recv', 'gethostbyname', 'getaddrinfo']
 
 #not a full list, but an educated list of common unsafe c++ funcs
 unsafe_functions = [
     'strcpy', 'strcat', 'sprintf', 'vsprintf',
     'gets', 'scanf', 'fscanf', 'sscanf',
-    'memcpy', 'memmove', 'memset',
     'strncpy', 'strncat', 'snprintf', 'vsnprintf',
     'fopen', 'freopen', 'tmpnam', 'tempnam', 'mktemp',
     'system', 'popen', 'unlink', 'remove', 'rename',
@@ -39,16 +39,14 @@ IO_functions = [
     'fopen', 'freopen', 'tmpnam', 'tempnam', 'mktemp',
     'system', 'popen', 'unlink', 'remove', 'rename',
     'gets', 'puts', 'read', 'write', 'open', 'close', 'fread', 'fwrite', 'fseek', 'ftell', 'rewind']
+
 file_operations = ['open', 'close', 'read', 'write', 'fread', 'fwrite', 'fseek', 'ftell', 'rewind']
     
-def is_unsafe_function(function):
-    return function.getName() in unsafe_functions
-
-def calls_unsafe_function(function):
+def contains_unsafe_function(function):
     references = getReferencesTo(function.getEntryPoint())
     for reference in references:
         caller_function = getFunctionContaining(reference.getFromAddress())
-        if caller_function and is_unsafe_function(caller_function):
+        if caller_function and caller_function.getName() in unsafe_functions:
             return True
     return False
 
@@ -63,28 +61,24 @@ def is_unused_function(function):
     references = getReferencesTo(function.getEntryPoint())
     return len(references) == 0
 
-def is_IO_function(function):
-    # check if any in func
+def contains_IO_function(function):
     references = getReferencesTo(function.getEntryPoint())
     for reference in references:
         caller_function = getFunctionContaining(reference.getFromAddress())
         if caller_function and caller_function.getName() in IO_functions:
             return True
 
-    # file operations check
-    function_name = function.getName()
-    for operation in file_operations:
-        if operation in function_name:
-            return True
+    return False
 
-    # Check if the function's body contains an I/O function call
-    instructions = listing.getInstructions(function.getEntryPoint(), True)
-    for instr in instructions:
-        mnemonic = instr.getMnemonicString()
-        if mnemonic in IO_functions:
+def contains_network_function(function):
+    references = getReferencesTo(function.getEntryPoint())
+    for reference in references:
+        caller_function = getFunctionContaining(reference.getFromAddress())
+        if caller_function and caller_function.getName() in networking_functions:
             return True
 
     return False
+
 
 def is_operation(function):
     # this should be safe because it is a reserved word in c++
@@ -98,22 +92,21 @@ def start():
 
     with open(outputPath, 'wb') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Function Name', 'Address', 'Is Unsafe', 'Calls Unsafe Function', 'Is Thunk', 'Is Unused', 'Is IO Function', 'Is Operation'])
+        csvwriter.writerow(['Function Name', 'Address', 'Contains Unsafe Function', 'Contains Thunk', 'Is Unused', 'Contains IO Function', 'Contains Network Function'])
 
         for function in functions:
             function_name = function.getName()
             function_address = function.getEntryPoint()
 
-            is_unsafe = is_unsafe_function(function)
-            calls_unsafe = calls_unsafe_function(function)
-            is_potential_thunk = is_thunk(function)
-            is_unused = is_unused_function(function)
-            is_IO = is_IO_function(function)
+            contains_unsafe = contains_unsafe_function(function)
             is_op = is_operation(function)
+            is_unused = is_unused_function(function)
+            is_th = is_thunk(function)
+            contains_IO = contains_IO_function(function)
+            contains_network = contains_network_function(function)
 
-            csvwriter.writerow([function_name, function_address, is_unsafe, calls_unsafe, is_potential_thunk, is_unused, is_IO, is_op])
+            csvwriter.writerow([function_name, function_address, contains_unsafe, is_th, is_unused, contains_IO, contains_network])
 
-	print("Export completed. CSV file saved to:", outputPath)
+    print("Export completed. CSV file saved to:", outputPath)
 
 start()
-
